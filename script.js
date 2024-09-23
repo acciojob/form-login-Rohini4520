5e2128841215585a6fbada7a07f58494f7356f6a
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2024-09-23 21:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getFormvalue() {
     //Write your code here
-	const fName = document.getElementById("fname");
-    const lName = document.getElementById("lname");
-	alert(fName.value +" "+ lName.value);
-
+	//alert(fName.value +" "+ lName.value);
+ const val = document.querySelectorAll("input");
+	let fName= val[0].value;
+	let lName = val[1].value;
+	alert(fName +" "+ lName);
 }
