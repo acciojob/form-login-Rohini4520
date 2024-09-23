675ed0525101f86fbda1af148522e2c4f31efb8b
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2024-09-23 21:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	const fname = document.getElementById(""fname");
+	const fname = document.getElementById("fname");
     const lname = document.getElementById("lname");
 	alert(fname.value +" "+ lname.value);
 
